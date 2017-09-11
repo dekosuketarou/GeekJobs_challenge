13004aa8f102160b69c01ffc0bb5a5306b9bd6e2
EXP-0013\guest1Day <dekosuke.tarou@gmail.com>
2017-09-11 11:09:39 +0900

    013.クラス/クラスの継承の課題を提出します

---
+++ 013.クラス/JavaClass2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaClass;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class JavaClass2 extends JavaClass1 {
+    public void clear(){
+        this.name="";
+        this.age=0;
+    }
+    
+}
